0b14097232cc2fbaf942cc4e208b46272ef6960b
Josue Rodriguez <jr2589213@gmail.com>
2020-02-20 15:52:57 -0800

    Initialize Repo - Initial commit > > * Created Player components in order to display on screen

---
+++ project/src/components/PlayerCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PlayerCard = () => {
+    return (
+        <div>
+            PlayerCard
+        </div>
+    )
+}
---
+++ project/src/components/PlayerList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import {PlayerCard} from './PlayerCard'
+
+export default function PlayerList() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <PlayerCard/>
+        </div>
+    )
+}
