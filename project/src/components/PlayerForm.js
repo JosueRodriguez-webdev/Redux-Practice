1e656b7be29a5b148c63a71c1b2967a534a81d20
Josue Rodriguez <jr2589213@gmail.com>
2020-02-20 17:35:01 -0800

    Add input form and add local state
    - made local state in /compnents/playerForm in  order to capture user input

--- project/src/components/PlayerForm.js
+++ project/src/components/PlayerForm.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {connect} from 'react-redux'
+import {postPlayer} from '../actions'
 
  function PlayerForm(props) {
+     const [player, setPlayer] = useState({
+         name: '',
+         rank: '',
+         nickname: ''
+     })
+
+    const inputHandler=e=> {
+        setPlayer({[e.target.name]: e.target.value})
+    }
+
+    const submitHandler =e =>{
+        e.preventDefault()
+        props.postPlayer(player)
+    }
+
     return (
         <div>
-            PlayerForm
+            <form id='playerForm' onSubmit={submitHandler}>
+                <label htmlFor='name'>Name</label>
+                    <input name='name' placeHolder = 'John' onChange={inputHandler} value={props.name}/>
+            </form>
+
+            <form id='playerForm'>
+                <label htmlFor='rank'>Name</label>
+                    <input name='rank' placeHolder = '#1' onChange={inputHandler} value={props.rank}/>
+            </form>
+
+            <form id='playerForm'>
+                <label htmlFor='nickname'>Name</label>
+                    <input name='nickname' placeHolder = 'lil john' onChange={inputHandler} value={props.nickname}/>
+
+                    <button type='submit' className='btn'>Submit</button>
+            </form>
             
         </div>
     )
@@ -16,4 +47,4 @@ export default connect(state=>{
        players: state.players,
        errors: state.errors 
     }
-},{})(PlayerForm)
+},{postPlayer})(PlayerForm)
