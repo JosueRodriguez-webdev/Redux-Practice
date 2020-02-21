d01dc3c5f9bad82b4b364443ecded73edde75942
Josue Rodriguez <jr2589213@gmail.com>
2020-02-20 18:30:53 -0800

    Finish reducer for action types

--- project/src/reducer/index.js
+++ project/src/reducer/index.js
@@ -1,3 +1,12 @@
+import {
+      POST_PLAYER,
+  POST_PLAYER_FAIL,
+  SUCCESS_POST_PLAYER,
+  GET_PLAYER,
+  GET_PLAYER_FAIL ,
+  SUCCESS_GET_PLAYER
+} from '../actions'
+
 // setup initialState
 
 const initialState = {
@@ -12,6 +21,31 @@ export const playerReducer = (state = initialState, action) => {
 
     //setup switch ,cases, and default
     switch(action.type){
+    case POST_PLAYER:
+        return {
+            ...state, isPosting: true
+        }
+    case POST_PLAYER_FAIL:
+        return {
+            ...state, isPosting: false, errors: action.payload
+        }
+    case SUCCESS_POST_PLAYER:
+        return {
+            ...state, isPosting: false, players: [action.payload]
+        }
+    case GET_PLAYER:
+        return {
+            ...state, isFetching: true
+        }
+    case GET_PLAYER_FAIL: 
+    return {
+        ...state, error: action.payload, isFetching: true
+    }
+    case SUCCESS_GET_PLAYER:
+        return {
+            ...state, players: [action.payload],
+            isFetching: false,
+        }
         default:
             return state
     }
