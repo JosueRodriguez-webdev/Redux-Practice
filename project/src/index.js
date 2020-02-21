9e94e0ced6ccd584f69058bb6d30cf6f3f116175
Josue Rodriguez <jr2589213@gmail.com>
2020-02-20 16:35:20 -0800

    Setup the provider and the store
    -wrapped app in project/index.js with the provider

--- project/src/index.js
+++ project/src/index.js
@@ -4,11 +4,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import {createStore} from 'redux'
-
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
+import {playerReducer} from './reducer'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(playerReducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
